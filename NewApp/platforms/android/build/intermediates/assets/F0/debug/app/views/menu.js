b580757714836364bfb5aaf88d56ee2dd2aebefe
LuisFranciscoCode <luisfranciscocode@gmail.com>
2017-03-22 16:59:19 +0000

    Revert "Just testing"
    This reverts commit 9569ce77cea64776c6dae701e5709595495d51e0.

--- NewApp/platforms/android/build/intermediates/assets/F0/debug/app/views/menu.js
+++ NewApp/platforms/android/build/intermediates/assets/F0/debug/app/views/menu.js
@@ -1,3 +1,7 @@
 exports.principal = function() {
-    console.info("ESTÁ A FUNCIONAR CARALHO");
+    //console.info("ESTÁ A FUNCIONAR CARALHO");
+    //TESTE
+    //Ora boas pessoal
+    //Daqui fala o Feromonas
+    // E eu tenho um segredo
 }
