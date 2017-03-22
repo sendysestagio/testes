ea9467121fd901b8ddacf0ab1576ee96f504a0e6
hugoroxas <hugo_roxas@live.com.pt>
2017-03-22 18:19:22 +0000

    Idea of application

--- NewApp/app/views/menu.js
+++ NewApp/app/views/menu.js
@@ -11,4 +11,44 @@ exports.principal = function() {
     // This is my branch ****
     // Round 2 is back
     // Xiu Lixo
+
+    /*
+
+    Case
+    if Json.type == "form"
+    then{
+        for(i = 0 ; i < Json.type.length ; i++)
+        {
+            Case
+            if Json.type[i] == "textbox"
+            then{
+                var textbox"[i]" = new TextBoxModule.TextBox;
+                textbox[i].id = Json.type[i].name;
+                textbox[i].val = Json.type[i].val;
+                textbox[i].text = Json.type[i].txt;
+                textbox[i].list = Json.type[i].list;
+            }
+
+            else if Json.type[i] == "combobox"
+            then{
+                var combobox"[i]" = new ComboBoxModule.ComboBox;
+                combobox[i].id = Json.type[i].name;
+                combobox[i].val = Json.type[i].val;
+                combobox[i].text = Json.type[i].txt;
+                combobox[i].list = Json.type[i].list;
+            }
+        }
+        
+    }
+
+    else if Json.type == "listview"
+    then{
+
+    }
+
+    .
+    .
+    .
+
+    */
 }
