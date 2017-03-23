8fe206f76a4d99648d6dfd1bf3ae6a91f565ec48
lbernasl <miguelbernardinoferreira@gmail.com>
2017-03-23 10:27:26 +0000

    change

--- NewApp/app/views/menu.js
+++ NewApp/app/views/menu.js
@@ -1,11 +1,18 @@
-exports.principal = function() {
-    //console.info("ESTÁ A FUNCIONAR CARALHO");
-    //TESTE
-    //Ora boas pessoal
-    //Daqui fala o Feromonas
-    // E eu tenho um segredo
+var page;
 
-    // testar cenas
-    // testando e bailando
-    // oi
+exports.principal = function(args){
+    page = args.Object;
+}
+
+exports.sou = function(){
+    fetch("http://10.0.7.102/teste/cores.json").then(response => { return response.json(); }).then(function (r) {
+        var jsonfile = r;
+     //   var bgpage = page.getElementByView("oi");
+        var redbg = jsonfile.red;
+        console.log(redbg);
+
+    //    bgpage.style.background = "#f00";
+
+        return redbg;
+    })
 }
