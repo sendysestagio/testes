c288829614ce74d4a3159728fe3db0e09ddcfec2
hugoroxas <hugo_roxas@live.com.pt>
2017-03-23 09:11:12 +0000

    Ideas

--- NewApp/app/views/menu.js
+++ NewApp/app/views/menu.js
@@ -1,18 +1,8 @@
 exports.principal = function() {
-    console.log("This is Hugo Branch");
-    //console.info("ESTÁ A FUNCIONAR CARALHO");
-    //TESTE
-    //Ora boas pessoal
-    //Daqui fala o Feromonas
-    // E eu tenho um segredo
-
-    // testar cenas
-    // testando e bailando
-    // This is my branch ****
-    // Round 2 is back
-    // Xiu Lixo
-
+    console.log("Welcom to Hugo's Branch");
     /*
+
+    Json Creation of Layout
 
     Case
     if Json.type == "form"
