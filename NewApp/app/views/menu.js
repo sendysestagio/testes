114efbf9c4847f4ef060b2601d7024b63cae2b54
LuisFranciscoCode <luisfranciscocode@gmail.com>
2017-03-23 10:29:57 +0000

    GridLayout with code
    just testing and works fine

--- NewApp/app/views/menu.js
+++ NewApp/app/views/menu.js
@@ -1,15 +1,56 @@
-exports.principal = function() {
-    //console.info("ESTÁ A FUNCIONAR CARALHO");
-    //TESTE
-    //Ora boas pessoal
-    //Daqui fala o Feromonas
-    // E eu tenho um segredo
+var labelModule = require("ui/label");
+var layout = require("ui/layouts/grid-layout");
 
-    // testar cenas
-    // testando e bailando
-    // This is my branch ****
-    // Round 2 is back
+exports.principal = function(args) {
 
-    // AGORA A BRANCH É MINHA CARALHO Luis
-    // zika zika
+    page = args.object;
+
+    var gridLayout = new layout.GridLayout();
+
+    var num_label = 0;
+    
+    for (cont = 0; cont <= 9; cont++) {
+        num_label += 1;
+
+        // array
+        var arraylbl = new Array(cont);
+
+        // array label
+        arraylbl[cont] = new labelModule.Label();
+
+        // text label
+        arraylbl[cont].text = "Text" + cont;
+
+        // name label
+        arraylbl[cont].className = "lbl" + cont;
+
+
+        // enquanto o contador é menor ou igual que 4 fica na linha 0
+        if (cont <= 4) {
+            layout.GridLayout.setColumn(arraylbl[cont], cont);
+        }
+
+        // quando o contador chegar a 5, a linha muda para 1
+        else if (cont == 5) {
+            num_label = 1;
+            layout.GridLayout.setRow(arraylbl[cont], 1);
+            num_label = 0;
+        }
+        else {
+            layout.GridLayout.setRow(arraylbl[cont], 1);    
+            layout.GridLayout.setColumn(arraylbl[cont], num_label);
+        }
+        
+        gridLayout.addChild(arraylbl[cont]);
+    
+        var column = new layout.ItemSpec(1, layout.GridUnitType.auto);
+        gridLayout.addColumn(column);
+
+        var row = new layout.ItemSpec(1, layout.GridUnitType.auto);    
+        gridLayout.addRow(row); 
+
+        console.info(arraylbl[cont].text);
+    }
+
+    page.content = gridLayout;
 }
