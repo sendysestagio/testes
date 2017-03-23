9f3f684fa2067c5c5a52974edb52270acc41d35f
hugoroxas <hugo_roxas@live.com.pt>
2017-03-23 11:09:16 +0000

    Hugo 23/03/2017 11:08

--- NewApp/app/views/menu.js
+++ NewApp/app/views/menu.js
@@ -1,5 +1,12 @@
-exports.principal = function() {
+var page;
+var layout = require("ui/layouts/grid-layout");
+var labelModule = require("ui/label");
+var buttonModule = require("ui/button");
+
+exports.principal = function(args) {
     console.log("Welcom to Hugo's Branch");
+    page = args.object;
+
     /*
 
     Json Creation of Layout
@@ -43,4 +50,51 @@ exports.principal = function() {
     page.MANDAR O LAYOUT**
 
     */
-}
+};
+
+exports.createTextView = function() {
+
+    var newGridLayout = new layout.GridLayout();
+    var label1 = new labelModule.Label();
+    var label2 = new labelModule.Label();
+    var label3 = new labelModule.Label();
+    var firstColumn = new layout.ItemSpec(1, layout.GridUnitType.auto);
+    var secondColumn = new layout.ItemSpec(1, layout.GridUnitType.auto);
+    var firstRow = new layout.ItemSpec(1, layout.GridUnitType.auto);
+    var secondRow = new layout.ItemSpec(1, layout.GridUnitType.auto);
+    var count = 0;
+
+    label1.text = "Olá";
+    label2.text = "Como vai?";
+    label3.text = "Tudo bem?";
+
+    layout.GridLayout.setColumn(label1, 0);
+    layout.GridLayout.setColumn(label2, 1);
+    layout.GridLayout.setRow(label3, 1);
+    layout.GridLayout.setColumn(label3, 2);
+    
+
+    fetch("http://10.0.7.102/teste/form.json").then(response => { return response.json(); }).then(function (r) {
+    
+    console.dump(r.fields);
+    console.dump(r.fields);
+    console.log(r.fields["0"].name);
+    console.log(r.fields[0].name);
+    console.log("oi");
+
+	});
+
+    newGridLayout.addChild(label1);
+    newGridLayout.addChild(label2);
+    newGridLayout.addChild(label3);
+    newGridLayout.addColumn(firstColumn);
+    newGridLayout.addColumn(secondColumn);
+    newGridLayout.addRow(firstRow);
+    newGridLayout.addRow(secondRow);
+
+    page.content = newGridLayout;
+
+    console.log("oi");
+
+ 
+};
--- NewApp/platforms/android/src/main/assets/app/views/menu.js
+++ NewApp/platforms/android/src/main/assets/app/views/menu.js
@@ -1,10 +1,100 @@
-exports.principal = function() {
-    //console.info("ESTÁ A FUNCIONAR CARALHO");
-    //TESTE
-    //Ora boas pessoal
-    //Daqui fala o Feromonas
-    // E eu tenho um segredo
+var page;
+var layout = require("ui/layouts/grid-layout");
+var labelModule = require("ui/label");
+var buttonModule = require("ui/button");
 
-    // testar cenas
-    // testando e bailando
-}
+exports.principal = function(args) {
+    console.log("Welcom to Hugo's Branch");
+    page = args.object;
+
+    /*
+
+    Json Creation of Layout
+
+    Case
+    if Json.type == "form"
+    then{
+        for(i = 0 ; i < Json.type.length ; i++)
+        {
+            Case
+            if Json.type[i] == "textbox"
+            then{
+                var textbox"[i]" = new TextBoxModule.TextBox;
+                textbox[i].id = Json.type[i].name;
+                textbox[i].val = Json.type[i].val;
+                textbox[i].text = Json.type[i].txt;
+                textbox[i].list = Json.type[i].list;
+            }
+
+            else if Json.type[i] == "combobox"
+            then{
+                var combobox"[i]" = new ComboBoxModule.ComboBox;
+                combobox[i].id = Json.type[i].name;
+                combobox[i].val = Json.type[i].val;
+                combobox[i].text = Json.type[i].txt;
+                combobox[i].list = Json.type[i].list;
+            }
+        }
+        
+    }
+
+    else if Json.type == "listview"
+    then{
+
+    }
+
+    .
+    .
+    .
+
+    page.MANDAR O LAYOUT**
+
+    */
+};
+
+exports.createTextView = function() {
+
+    var newGridLayout = new layout.GridLayout();
+    var label1 = new labelModule.Label();
+    var label2 = new labelModule.Label();
+    var label3 = new labelModule.Label();
+    var firstColumn = new layout.ItemSpec(1, layout.GridUnitType.auto);
+    var secondColumn = new layout.ItemSpec(1, layout.GridUnitType.auto);
+    var firstRow = new layout.ItemSpec(1, layout.GridUnitType.auto);
+    var secondRow = new layout.ItemSpec(1, layout.GridUnitType.auto);
+    var count = 0;
+
+    label1.text = "Olá";
+    label2.text = "Como vai?";
+    label3.text = "Tudo bem?";
+
+    layout.GridLayout.setColumn(label1, 0);
+    layout.GridLayout.setColumn(label2, 1);
+    layout.GridLayout.setRow(label3, 1);
+    layout.GridLayout.setColumn(label3, 2);
+    
+
+    fetch("http://10.0.7.102/teste/form.json").then(response => { return response.json(); }).then(function (r) {
+    
+    console.dump(r.fields);
+    console.dump(r.fields);
+    console.log(r.fields["0"].name);
+    console.log(r.fields[0].name);
+    console.log("oi");
+
+	});
+
+    newGridLayout.addChild(label1);
+    newGridLayout.addChild(label2);
+    newGridLayout.addChild(label3);
+    newGridLayout.addColumn(firstColumn);
+    newGridLayout.addColumn(secondColumn);
+    newGridLayout.addRow(firstRow);
+    newGridLayout.addRow(secondRow);
+
+    page.content = newGridLayout;
+
+    console.log("oi");
+
+ 
+};
