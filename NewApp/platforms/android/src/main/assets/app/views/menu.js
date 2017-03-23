a26652168404e31b53afa804b2058bddb5eba7de
LuisFranciscoCode <luisfranciscocode@gmail.com>
2017-03-23 17:04:48 +0000

    Grid and Stack

--- NewApp/platforms/android/src/main/assets/app/views/menu.js
+++ NewApp/platforms/android/src/main/assets/app/views/menu.js
@@ -1,6 +1,9 @@
 var labelModule = require("ui/label");
-// var layout = require("ui/layouts/grid-layout");
+var layout = require("ui/layouts/grid-layout");
 var stackLayout = require("ui/layouts/stack-layout");
+var buttonModule = require("ui/button");
+
+var x = 0;
 
 exports.principal = function(args) {
     page = args.object;
@@ -17,42 +20,85 @@ readJson = function() {
         var numTypes = Object.keys(valor.fields).length;
         var numCol = numTypes;
 
-        drawGrid(valor, numTypes);
-
+        switch (x) {
+            case 0:
+                drawStack(valor, numTypes);
+                break;
+            case 1:
+                drawGrid(valor, numTypes);
+                break;
+        }      
 	});    
 }
 
 drawGrid = function(jsonValue, numCol) {
-   
-   var stack = new stackLayout.StackLayout();
 
-   // var gridLayout = new layout.GridLayout();
+    var gridLayout = new layout.GridLayout();
     var num_label = 0;
-    
+
     for (cont = 0; cont < numCol; cont++) {
         var arraylbl = new Array(cont);
         arraylbl[cont] = new labelModule.Label();
         arraylbl[cont].text = jsonValue.fields[cont].type ;
         arraylbl[cont].className = "lbl" + cont;
         arraylbl[cont].backgroundColor = "#0f0";
+        arraylbl[cont].width = 100; 
 
-        // layout.GridLayout.setColumn(arraylbl[cont], num_label);
-        
-       // gridLayout.addChild(arraylbl[cont]);
+        layout.GridLayout.setColumn(arraylbl[cont], num_label);
+        gridLayout.addChild(arraylbl[cont]);
     
-      //  var column = new layout.ItemSpec(1, layout.GridUnitType.auto);
-      //  gridLayout.addColumn(column);
+        var column = new layout.ItemSpec(1, layout.GridUnitType.auto);
+        gridLayout.addColumn(column);
 
-     //   var row = new layout.ItemSpec(1, layout.GridUnitType.auto);    
-       // gridLayout.addRow(row); 
+        var row = new layout.ItemSpec(1, layout.GridUnitType.auto);    
+        gridLayout.addRow(row);        
+        num_label += 1;
+   }
 
-      stack.addChild(arraylbl[cont]);
+   var btn = new buttonModule.Button();
+    btn.text = "Change Layout";
+    btn.on(buttonModule.Button.tapEvent, function() {
+        changeLayout();
+    });
 
+    layout.GridLayout.setRow(btn, 1);
+    gridLayout.addChild(btn);
 
+    gridLayout.backgroundColor = "#7FFFD4";
+    page.content = gridLayout;
+}
+
+drawStack = function(jsonValue, numCol) {
+    var stack = new stackLayout.StackLayout();
+    var num_label = 0;
+
+    for (cont = 0; cont < numCol; cont++) {
+        var arraylbl = new Array(cont);
+        arraylbl[cont] = new labelModule.Label();
+        arraylbl[cont].text = jsonValue.fields[cont].type ;
+        arraylbl[cont].className = "lbl" + cont;
+        arraylbl[cont].backgroundColor = "#0f0";
+        arraylbl[cont].width = 100;
+        stack.addChild(arraylbl[cont]);
         num_label += 1;
     }
 
-    // gridLayout.backgroundColor = "#7FFFD4";
-   // page.content = gridLayout;
+    var btn = new buttonModule.Button();
+    btn.text = "Change Layout";
+    btn.on(buttonModule.Button.tapEvent, function() {
+       changeLayout();
+    });
+
+    stack.addChild(btn);
     page.content = stack;
+}
+
+changeLayout = function() {
+    if (x == 0) {
+        x = 1;
+    }
+    else if (x == 1) {
+        x = 0;
+    }
+    readJson();
 }
