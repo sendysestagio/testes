152d5d02d810e46ec7993f9a9d10781401735cf0
lbernasl <miguelbernardinoferreira@gmail.com>
2017-03-23 11:55:22 +0000

    rerun android

--- NewApp/platforms/android/build/intermediates/assets/F0/debug/app/views/menu.js
+++ NewApp/platforms/android/build/intermediates/assets/F0/debug/app/views/menu.js
@@ -1,7 +1,16 @@
-exports.principal = function() {
-    //console.info("ESTÁ A FUNCIONAR CARALHO");
-    //TESTE
-    //Ora boas pessoal
-    //Daqui fala o Feromonas
-    // E eu tenho um segredo
+var pageviewmodel = require("../views/menu");
+var pageview = new pageviewmodel([]);
+
+exports.principal = function(args){
+    page = args.Object;
+}
+
+exports.sou = function(){
+    fetch("http://10.0.7.102/teste/cores.json").then(response => { return response.json(); }).then(function (r) {
+        var jsonfile = r;
+        var redbg = jsonfile.red;
+        console.log(redbg);
+
+        pageview.background = redbg;
+    })
 }
--- NewApp/platforms/android/src/main/assets/app/views/menu.js
+++ NewApp/platforms/android/src/main/assets/app/views/menu.js
@@ -1,4 +1,4 @@
-var pageviewmodel = require("./views/menu");
+var pageviewmodel = require("../views/menu");
 var pageview = new pageviewmodel([]);
 
 exports.principal = function(args){
@@ -8,7 +8,6 @@ exports.principal = function(args){
 exports.sou = function(){
     fetch("http://10.0.7.102/teste/cores.json").then(response => { return response.json(); }).then(function (r) {
         var jsonfile = r;
-
         var redbg = jsonfile.red;
         console.log(redbg);
 
