53d2df74872c3395c49038eb48b5fc8418577f03
hugoroxas <hugo_roxas@live.com.pt>
2017-03-24 18:22:38 +0000

    New Programing skills not unlock !

--- NewApp/app/views/menu.js
+++ NewApp/app/views/menu.js
@@ -14,20 +14,13 @@ exports.principal = function(args) {
 };
 
 
-exports.ola = function() {
+function ola() {
     alert("oi");
 }
 
 exports.createTextView = function() {
 
     fetch("http://10.0.7.102/teste/form2.json").then(response => { return response.json(); }).then(function (r) {
-        
-        var f = '{ "nome":"hugo", "idade":17 }';
-        console.dump(f);
-        var myJSON2 = JSON.parse(f);
-        console.dump(myJSON2);
-        console.log(myJSON2.nome);
-        console.log(myJSON2.idade);
         
         var myJSON = r;
         var fieldsSize = myJSON.fields.length;
@@ -103,7 +96,8 @@ exports.createTextView = function() {
                                                 buttonArray[i].txt = myJSON.fields[i].txt;
                                                 buttonArray[i].List = myJSON.fields[i].List;
 
-                                                buttonArray[i].on(buttonModule.Button.tapEvent, buttonArray[i].func );
+                                                console.log(myJSON.fields[i].func + "()" );
+                                                buttonArray[i].on(buttonModule.Button.tapEvent, myJSON.fields[i].func + "()" );
 
                                                 newStackLayout.addChild(buttonArray[i]);
 
